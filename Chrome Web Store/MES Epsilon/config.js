b49b6c8f333440891aeafbde145082e156e75b99
ChrisMagnuson <hourglasssand@gmail.com>
2017-12-21 10:45:05 -0500

    Initial commit of Chrome web store app for MES

---
+++ Chrome Web Store/MES Epsilon/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "MES Epsilon",
+   "homepage": "http://mesiis.epsilon.tervis.prv/",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
---
+++ Chrome Web Store/MES Production/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "MES",
+   "homepage": "http://mesiis.production.tervis.prv/",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
